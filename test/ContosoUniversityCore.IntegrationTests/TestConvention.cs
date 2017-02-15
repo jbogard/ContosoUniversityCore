2b2555168b5b381d116b9f20e5808ddf1615079d
Alexander Sidorov <Alexander Sidorov>
2017-02-15 23:22:17 +0700

    Cleaned Fixie configuration

---
+++ test/ContosoUniversityCore.IntegrationTests/TestConvention.cs
@@ -0,0 +1,13 @@
+﻿using Fixie.Conventions;
+
+namespace ContosoUniversityCore.IntegrationTests
+{
+    public class TestConvention : DefaultConvention
+    {
+        public TestConvention()
+        {
+            FixtureExecution
+                .Wrap<DeleteData>();
+        }
+    }
+}
